062d6e5d67103bfd95ef536381731c6a74d87804
Emilio De Leon <emiliodeleon.contact@gmail.com>
2021-09-30 23:57:21 -0500

    Añadiendo CRUD!

--- js/admin.js
+++ js/admin.js
@@ -2,15 +2,18 @@ let formularioAgregar = document.querySelector("#form-add_product")
 formularioAgregar.addEventListener("submit", (event)=> {
 
     event.preventDefault()
-    console.log(event.target)
-    console.log(formularioAgregar)
-    let producto = {}
+    let producto = {tamanos:[]}
     formularioAgregar.querySelectorAll(".form-control").forEach((input)=> {
-        console.log(input.name)
-        console.log(input.value)
+        if(input.type === "checkbox"){
+            if(input.checked){
+                producto.tamanos = [...producto.tamanos,input.value] // Asignacion propiedad tamanos con spread operator 
+            }
+        }else{
+            producto[input.name] = input.value // 
+        }
+
     })
-
-
+    agregarProducto(producto)
 })
 
 // Funcion para Create
@@ -28,3 +31,53 @@ const agregarProducto = (producto) => {
     xhr.send(JSON.stringify(producto))
 }
 
+// Funcion para Traer de la base de datos (Read)
+
+const getProducts = () => {
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("readystatechange", () =>{
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                let descargBD = xhr.responseText
+                let bdParsed = (JSON.parse(descargBD))
+                console.log(bdParsed)
+                alert("Descarga exitosa")
+            }
+        }
+
+    })
+    xhr.open("GET", "https://api-friki-mkt-default-rtdb.firebaseio.com/.json",true)
+    xhr.send()
+}
+
+// Funcion para actualizar productos (Update)
+
+const updateProducts = (idProducto, objetoUpdate = {productoName:"Barbie"}) => {
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("readystatechange", () =>{
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                alert("Updaate exitoso")
+            }
+        }
+
+    })
+    xhr.open("PATCH", `https://api-friki-mkt-default-rtdb.firebaseio.com/${idProducto}.json`,true)
+    xhr.send(JSON.stringify(objetoUpdate))
+}
+
+// Funcion para eliminar productos
+
+const deleteProducts = (idProducto) => {
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("readystatechange", () =>{
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                alert("Delete exitoso")
+            }
+        }
+
+    })
+    xhr.open("DELETE", `https://api-friki-mkt-default-rtdb.firebaseio.com/${idProducto}.json`,true)
+    xhr.send()
+}
